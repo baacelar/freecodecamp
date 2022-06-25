7a0d820ca34d64398e2fed0c8b681a7bbc98132a
baacelar <heriton.alves@ufba.br>
2022-06-25 12:53:57 -0300

    Uploading new answers

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
-let mercado = [{ 
-    nome:"Maçã", 
-    preco: 5,
-}, 
-{ 
-    nome:'Banana', 
-    preco: 3.9,
-},  
-{ 
-    nome:'Melancia', 
-    preco: 7, 
-}
+// let mercado = [{ 
+//     nome:"Maçã", 
+//     preco: 5,
+// }, 
+// { 
+//     nome:'Banana', 
+//     preco: 3.9,
+// },  
+// { 
+//     nome:'Melancia', 
+//     preco: 7, 
+// }
 
-]  
+// ]  
 
-console.table(mercado)
+// console.table(mercado)  
+
+let sum = 0; 
+
+function addSum(num){ 
+    return sum = sum + num;
+} 
+
+
+console.log(addSum(3)); 
